ebb1e76a7bae804f272e3d9772ce421ce9b460c7
Ahalya-KT <ahalyakt002@gmail.com>
2023-10-23 10:23:53 +0530

    fashion

--- src/components/Sales.jsx
+++ src/components/Sales.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
-import img from '../assets/images/pic9.jpg'
+
 
 const Sales = () => {
   return (
     <div className='pt-44'>
-      <div className="bg-[url('../assets/images/pic9.jpg')] w-full  bg-cover">
-
-        hello
-
+      <div className="bg-[url('./assets/images/pic9.jpg')] w-screen h-96 bg-cover">
+        <div className='mx-2.5'>
+        <p>PAYDAY</p>
+        <p>SALE NOW</p>
+        <p>Spend minimal $100 get 30% off voucher code for next purchase</p>
+        <p>1 june-10 june 2021</p>
+        <p>*Terms $ Condition apply</p>
+        </div>
+                   
       </div>
     </div>
   )
