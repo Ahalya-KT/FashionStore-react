a552f4095d1273ac3a6d5a1ce837197952455182
Ahalya-KT <ahalyakt002@gmail.com>
2023-10-23 10:46:17 +0530

    fashion

--- src/components/Sales.jsx
+++ src/components/Sales.jsx
@@ -4,11 +4,11 @@ import React from 'react'
 const Sales = () => {
   return (
     <div className='pt-44'>
-      <div className="bg-[url('./assets/images/pic9.jpg')] w-screen h-96 bg-cover">
-        <div className='mx-2.5'>
+      <div className="bg-[url('./assets/images/pic9.jpg')] w-screen h-96 bg-cover text-right pl-4 mr-96">
+        <div className='text-right mr-56  '>
         <p>PAYDAY</p>
         <p>SALE NOW</p>
-        <p>Spend minimal $100 get 30% off voucher code for next purchase</p>
+        <p className='w-full break-all'>Spend minimal $100 get 30% off voucher code for next purchase</p>
         <p>1 june-10 june 2021</p>
         <p>*Terms $ Condition apply</p>
         </div>
