11c28ea3aaa48a43aa75efa267f3a26b60bdcd80
Ahalya-KT <ahalyakt002@gmail.com>
2023-10-30 11:53:26 +0530

    fashion

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import Button from '../layouts/Button'
 const Navbar = () => {
   return (
     <div>
-      <div className='flex flex-row justify-between p-4 px-32 pt-6 items-center bg-white h-12'>
+      <div className='flex flex-row justify-between p-4 px-32 pt-6 items-center  bg-white h-12'>
         <div className='font-bold -tracking-2 '>
         <Link to="/" className='flex flex-row gap-1' >
           <SiShopify size={20}/> 
@@ -14,7 +14,7 @@ const Navbar = () => {
         </Link>
         </div>
         
-        <nav className='space-x-6 text-sm flex flex-row   '>
+        <nav className='space-x-6 text-sm flex flex-row items-center  '>
             <Link to="home">CATALOGUE</Link>
             <Link to="home">FASHION</Link>
             <Link to="home">FAVOURITE</Link>
