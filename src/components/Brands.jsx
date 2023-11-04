f8bf9acc2a86d67e80695c353de1c6e7405904fc
Ahalya-KT <ahalyakt002@gmail.com>
2023-11-04 13:32:36 +0530

    fashion

--- src/components/Brands.jsx
+++ src/components/Brands.jsx
@@ -3,19 +3,19 @@ const Brands = () => {
   return (
     <div>
       <div className='bg-amber-300 h-16'>
-       <div className='flex flex-row w-16 p-2 gap-32  items-center ml-28'>
+       <div className='flex flex-row w-16 p-2  gap-32  items-center ml-28'>
 
-      <img className='pl-5 ' src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/H%26M-Logo.svg/2560px-H%26M-Logo.svg.png"></img>
+      <img className='pl-5 ' src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/H%26M-Logo.svg/2560px-H%26M-Logo.svg.png"/>
 
-      <img src="https://1000logos.net/wp-content/uploads/2017/08/Obey-Logo.png"></img>
+      <img src="https://1000logos.net/wp-content/uploads/2017/08/Obey-Logo.png"/>
 
-      <img src="https://res.cloudinary.com/hevo/image/upload/c_scale,w_215,h_61,dpr_2/f_auto,q_auto/v1685910977/hevo-learn-1/215px-Shopify_logo_2018.svg_.png"></img>
+      <img  className ="h-7"src="https://res.cloudinary.com/hevo/image/upload/c_scale,w_215,h_61,dpr_2/f_auto,q_auto/v1685910977/hevo-learn-1/215px-Shopify_logo_2018.svg_.png"/>
 
-      <img  className="pt-2"src="https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg"></img>
+      <img   className ="h-7" src="https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg"/>
 
-      <img src="https://assets.stickpng.com/images/585990234f6ae202fedf28cf.png"></img>
-
-      <img src="https://upload.wikimedia.org/wikipedia/commons/3/33/Meesho_logo.png"></img>
+      <img className='h-10' src="https://www.freepnglogos.com/uploads/flipkart-logo-png/flipkart-com-logo-internet-ltd-state-of-kerala-10.png"/>
+      
+      <img  src="https://upload.wikimedia.org/wikipedia/commons/3/33/Meesho_logo.png"/>
        </div>
       </div>
     </div>
--- src/components/Sales.jsx
+++ src/components/Sales.jsx
@@ -5,7 +5,7 @@ import Button from '../layouts/Button'
 const Sales = () => {
   return (
     <div className='pt-44'>
-      <div className="bg-[url('./assets/images/pic9.jpg')] w-screen h-96 bg-cover  ">
+      <div className="bg-[url('./assets/images/pic9.jpg')] h-96 bg-cover  ">
         <div className='float-right pt-16 '>
         <p className='font-bold text-5xl mr-48  bg-white w-60 px-4'>PAYDAY</p>
         <p className='font-bold text-5xl'>SALE NOW</p>
